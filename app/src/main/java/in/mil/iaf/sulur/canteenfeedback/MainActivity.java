ac51d4297886b91f2416478032977152095dee80
Parth Saini <slrparth@amazon.com>
2026-05-13 23:13:46 +0530

    fix: Add missing class declaration in MainActivity

--- app/src/main/java/in/mil/iaf/sulur/canteenfeedback/MainActivity.java
+++ app/src/main/java/in/mil/iaf/sulur/canteenfeedback/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 
+public class MainActivity extends AppCompatActivity {
+
     private FeedbackDbHelper dbHelper;
     private EditText editName, editPhone, editComments;
     private RatingBar ratingFood, ratingService, ratingCleanliness, ratingValue;
